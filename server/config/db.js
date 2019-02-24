0fb4a5b33670295b50c07fa95210f8b690e7de6d
Ryan <100638060@oncampus.local>
2019-02-23 23:16:32 -0500

    dummy

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb://localhost/3000"
+  //Remote MongoDB deployment -> let 
+  //"URI" :"mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
+};
